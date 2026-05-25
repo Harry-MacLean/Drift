9bc7d6f6c1b78859ea808a67c76e1da7df096c92
Harry-MacLean <harryjohnmaclean@gmail.com>
2026-05-25 21:31:44 +1200

    Finalise detect_move() - output, rounding, date format

--- detect_move().py
+++ detect_move().py
@@ -1,5 +1,5 @@
 import yfinance as yf
-
+import pandas as pd
 
 
 def detect_move(ticker, threshold=2):
@@ -7,7 +7,12 @@ def detect_move(ticker, threshold=2):
     data = stock.history(period="1mo")
     drift = data["Close"].pct_change() * 100
     significant_moves = drift[(drift >= threshold) | (drift <= -threshold)]
-    return significant_moves
+    df = significant_moves.to_frame()
+    df.rename(columns={"Close": "PCT Change %"}, inplace=True)
+    df.index = df.index.date
+    df = df.round(3)
+    df["ticker"] = ticker
+    return df
 
-result = detect_move("AIR.NZ")
-print(result)
+result = detect_move("ATM.NZ")
+result.to_csv("significant_moves.csv")
